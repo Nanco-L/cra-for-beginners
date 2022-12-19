eab3ee4a0bf3fb7b55c772263f276cf5d8c4ee70
Kyuhyun Lee <khlee1992@naver.com>
2022-12-20 00:10:14 +0900

    Until lecture #6

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-    const [value, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("i run all the time");
+function Hello() {
     useEffect(() => {
-        console.log("CALL THE API...");
+        console.log("Created :)");
+        return () => console.log("Destroyed :(");
     }, []);
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("SEARCH FOR", keyword);
-        }
-    }, [keyword]);
+    return <h1>Hello</h1>;
+}
 
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            />
-            <h1 className={styles.title}>{value}</h1>
-            <button onClick={onClick}>Click Me!</button>
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+            {showing ? <Hello /> : null}
         </div>
     );
 }
