3c5a3b39ccfdf474a6b69f1917fec3fe1a79a494
Kyuhyun Lee <khlee1992@naver.com>
2022-12-21 22:43:02 +0900

    Until lecture #7.1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-    useEffect(() => {
-        console.log("Created :)");
-        return () => console.log("Destroyed :(");
-    }, []);
-    return <h1>Hello</h1>;
-}
-
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
     return (
         <div>
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-            {showing ? <Hello /> : null}
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do"
+                />
+                <button>Add To Do</button>
+            </form>
         </div>
     );
 }
